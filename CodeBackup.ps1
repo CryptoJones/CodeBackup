d1bcef34bcbb65b9158466ad974cdb6faed16f80
Aaron K. Clark <cryptojones@owasp.org>
2026-05-13 21:50:27 -0500

    security: replace hardcoded credentials with environment variables

--- CodeBackup.ps1
+++ CodeBackup.ps1
@@ -1,84 +1,49 @@
-$dteToday = Get-Date
+# CodeBackup.ps1
+# Credentials and paths loaded from environment variables.
+# Never hardcode passwords in scripts committed to source control.
+$NetworkPath = $env:BACKUP_NETWORK_PATH
+$NetUser     = $env:BACKUP_NET_USER
+$NetPass     = $env:BACKUP_NET_PASS
+
+if (-not $NetworkPath -or -not $NetUser -or -not $NetPass) {
+    Write-Error "Set BACKUP_NETWORK_PATH, BACKUP_NET_USER, and BACKUP_NET_PASS before running."
+    exit 1
+}
+
+$dteToday           = Get-Date
 $strCurrentFileDate = $dteToday.ToString("yyyy-MM-dd")
-$strCurrentMonth = $strCurrentFileDate.Substring(5, 2)
-$strCurrentFileName = $strCurrentFileDate + ".7z"
-$strFullPath = "S:\" + $strCurrentFileName
+$strCurrentMonth    = [int]$strCurrentFileDate.Substring(5, 2)
+$intCurrentYear     = [int]$strCurrentFileDate.Substring(0, 4)
+$strFullPath        = "S:\" + $strCurrentFileDate + ".7z"
 
 Import-Module C:\bin\7Zip4PowerShell.dll
 
-(New-Object -ComObject WScript.Network).MapNetworkDrive('S:','\\server01\PATH\PATH2\PATH3',$true,'domain\user','IL0veG0@tS')
-If (Test-Path $strFullPath){
-	Remove-Item $strFullPath
+(New-Object -ComObject WScript.Network).MapNetworkDrive("S:", $NetworkPath, $true, $NetUser, $NetPass)
+
+If (Test-Path $strFullPath) { Remove-Item $strFullPath }
+
+$intPreviousMonth = $strCurrentMonth - 2
+switch ($intPreviousMonth) {
+    0  { $intCurrentYear--; $intPreviousMonth = 12 }
+    -1 { $intCurrentYear--; $intPreviousMonth = 11 }
 }
 
-$intPreviousMonth = $strCurrentMonth - 2 
-$intCurrentYear = $strCurrentFileDate.Substring(0, 4)
+$strPreviousMonth = $intPreviousMonth.ToString("D2")
+$strCurrentYear   = $intCurrentYear.ToString()
 
-If ($intPreviousMonth -lt 1){
-    switch ($intPreviousMonth){
-        0 {
-            $intCurrentYear = $intCurrentYear - 1
-            $intPreviousMonth = 12
-        }
-        -1 {
-            $intCurrentYear = $intCurrentYear - 1
-            $intPreviousMonth = 11
-        }
-    }
-} else {
-    $strCurrentYear = $intCurrentYear.ToString()
+$sources = @{
+    db  = "C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\Backup"
+    svn = "C:\Source"
+    www = "C:\inetpub"
 }
 
-$strPreviousMonth = ""
-
-If ($intPreviousMonth -lt 10) {
-    $strPreviousMonth = "0" + $intPreviousMonth
-} Else {
-    $strPreviousMonth = $intPreviousMonth
+foreach ($type in $sources.Keys) {
+    $archive = $strFullPath -replace "\.7z", ".$type.7z"
+    $oldGlob = "S:\$strCurrentYear-$strPreviousMonth*.$type.7z"
+    Compress-7Zip -ArchiveFileName $archive $sources[$type]
+    Remove-Item $oldGlob -ErrorAction SilentlyContinue
+    Write-Output "Backed up $type -> $archive (pruned: $oldGlob)"
 }
 
-# Datbase backups #
-
-$strLocalPath =  $strFullPath -replace ".7z", ".db.7z"
-
-$strFilePatternToDelete = $strCurrentYear + "-" + $strPreviousMonth + "*.7z"
-$strRemoveItemParam = "S:\" + $strFilePatternToDelete
-$strRemoveItemParam  = $strRemoveItemParam -replace ".7z", ".db.7z"
- 
-Compress-7Zip -ArchiveFileName $strLocalPath "C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\Backup"
- 
-Remove-Item $strRemoveItemParam
- 
-$strMessage = "Deleting " + $strRemoveItemParam
-echo $strMessage
-
-# Svn Backups #
- 
-$strFilePatternToDelete = $strCurrentYear + "-" + $strPreviousMonth + "*.7z"
-$strRemoveItemParam = "S:\" + $strFilePatternToDelete
-$strRemoveItemParam  = $strRemoveItemParam -replace "db.7z", ".svn.7z"
-
-$strLocalPath =  $strFullPath -replace ".7z", ".svn.7z"
- 
-Compress-7Zip -ArchiveFileName $strLocalPath "C:\Source"
- 
-Remove-Item $strRemoveItemParam
- 
-$strMessage = "Deleting " + $strRemoveItemParam
-echo $strMessage
-
-# Web backups #
-
-$strLocalPath =  $strFullPath -replace ".7z", ".www.7z"
-
-$strFilePatternToDelete = $strCurrentYear + "-" + $strPreviousMonth + "*.7z"
-$strRemoveItemParam = "S:\" + $strFilePatternToDelete
-$strRemoveItemParam  = $strRemoveItemParam -replace ".svn.7z", ".www.7z"
- 
-Compress-7Zip -ArchiveFileName $strLocalPath "C:\inetpub"
- 
-Remove-Item $strRemoveItemParam
- 
-$Drive = Get-WmiObject -Class Win32_mappedLogicalDisk -filter "ProviderName='\\\\server01\\PATH\\PATH2\\PATH3'"
-
-net use $Drive.Name /delete /y
+$drive = Get-WmiObject -Class Win32_MappedLogicalDisk -Filter "ProviderName='$NetworkPath'"
+net use $drive.Name /delete /y
